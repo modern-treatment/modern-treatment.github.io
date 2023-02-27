61fe49aecadb7c24213c7b34ed819fae315bbb0c
vlad <vlad.bras02@gmail.com>
2023-02-27 14:15:02 +0100

    Cardione RO

---
+++ d71d88b8/js/translater.js
@@ -0,0 +1,247 @@
+var new_lang = {
+    
+"lt0": "<img alt=\"icon\" src=\"img/icon1.png\">Aprobat de <br>cardiologi renumiți",
+
+"lt1": "<img alt=\"icon\" src=\"img/icon2.png\">Compoziție <br>naturală — 100%",
+
+"lt2": "<img alt=\"icon\" src=\"img/icon3.png\">Efect<br>clinic dovedit",
+
+"lt3": "pentru a învinge rapid hipertonia și simptomele ei",
+
+"lt4": " <span>Normalizează </span>tensiunea în mod natural",
+
+"lt5": " <span>Restabilește</span> tonusul și elasticitatea vasculară",
+
+"lt6": " <span>Previne </span>atacurile de cord și accidentele vascular-cerebrale",
+
+"lt7": " <span>Este eficient</span> la orice etapă a hipertoniei",
+
+"lt8": "Comandă",
+
+"lt9": "OFERTĂ SPECIALĂ",
+
+"lt10": "până la sfârșitul promoției a mai rămas",
+
+"lt11": "COMANDĂ",
+
+"lt12": "SIMPTOMELE HIPERTONIEI AVANSATE",
+
+"lt13": "probleme cu <br>somnul",
+
+"lt14": "dereglări <br>ale memoriei",
+
+"lt15": "edem <br>dmineața",
+
+"lt16": "dureri de cap și <br>amețeli",
+
+"lt17": "oboseală <br>cronică",
+
+"lt18": "dificultăți de respirație, <br>dureri acute în piept",
+
+"lt19": "sunet strident în urechi<br><br>",
+
+"lt20": "anxietate, <br>iritabilitate",
+
+"lt21": "transpirație <br>excesivă",
+
+"lt22": "HIPERTENSIUNEA ESTE UN PERICOL <br>PENTRU ÎNTREGUL ORGANISM!",
+
+"lt23": "INIMĂ",
+
+"lt24": "cardiomegalie<i></i>",
+
+"lt25": "cardiomiopatia<i></i>",
+
+"lt26": "Infarct miocardic<i></i>",
+
+"lt27": "FICAT",
+
+"lt28": "ciroză<i></i>",
+
+"lt29": "tulburare metabolică<i></i>",
+
+"lt30": "înrăutățirea stării funcționale<i></i>",
+
+"lt31": "SISTEMUL <br>SANGVIN",
+
+"lt32": "subțierea pereților <br>vaselor și arteriilor<i></i>",
+
+"lt33": "diabet<i></i>",
+
+"lt34": "CREIER",
+
+"lt35": "<i></i>disfuncții ale circulației sangvine în creier",
+
+"lt36": "<i></i>scăderea memoriei și intelectului",
+
+"lt37": "<i></i>accident cerebrovascular ",
+
+"lt38": "VEDERE",
+
+"lt39": "<i></i>detașare retinei",
+
+"lt40": "<i></i>hemoragie",
+
+"lt41": "<i></i>orbirea",
+
+"lt42": "RINICHI",
+
+"lt43": "<i></i>nefroscleroză",
+
+"lt44": "<i></i>insuficiență renală",
+
+"lt45": "<span class=\"red-bck\">XX</span> ÎNLĂTURĂ <span class=\"color-red\">4 CAUZE </span>PRINCIPALE <br>ALE HIPERTONIEI",
+
+"lt46": "TENSIUNEA NERVOASĂ ,MARITA",
+
+"lt47": "Combate anxietatea și reglează somnul și starea trează.",
+
+"lt48": "CONTAMINAREA VASELOR",
+
+"lt49": "Curăță pereții vaselor sangvine de plăcile de colesterol, prevenind ateroscleroza",
+
+"lt50": "Cantitatea mărită de zahăr <br>în sânge",
+
+"lt51": "Duce la scăderea zahărului din sânge, împiedicând dezvoltarea diabetului",
+
+"lt52": "GREUTATEA EXCESIVĂ",
+
+"lt53": "Contribuie la normalizarea greutății din contul îmbunătățirii metabolismului",
+
+"lt54": "Comandă",
+
+"lt55": "COMPOZIȚIA NATURALĂ A <span class=\"red-bck\">XX</span> INCLUDE<br> COMPONENTE CU BIODISPONIBILITATE RIDICATĂ:",
+
+"lt56": "TIAMINĂ",
+
+"lt57": "îmbunătățește contracțiile inimii și asigură sănătatea miocardului. Previne anemia, stabilizează metabolismul grăsimilor și asigură funcționarea normală a vaselor creierului și ochilor.",
+
+"lt58": "SELENIU",
+
+"lt59": "nu numai că protejează vasele de sânge de apariția aterosclerozei, ci și direct mușchiul inimii în sine de efectele nocive ale radicalilor liberi.",
+
+"lt60": "VITAMINA C",
+
+"lt61": "garantarea elasticității pereților vaselor sangvine. Datorită acțiunii sale, ei nu se supun ruperii și fragilității. De asemenea, îmbunătățește coagularea sângelui, accelerează repararea țesuturilor.",
+
+"lt62": "TERMINALIA ARJUNA",
+
+"lt63": "tonic puternic pentru inimă, tonifiază inima și plămânii, stimulează circulația coronariană, întărește mușchiul cardiac.",
+
+"lt64": "EXTRACT DIN SÂMBURI DE STRUGURI",
+
+"lt65": "stimulează inima și sistemul circulator, reduce riscul de accident vascular cerebral. Întărește pereții vaselor sangvine și sporește elasticitatea acestora.",
+
+"lt66": "EXTRACTE DE PLANTE",
+
+"lt67": "efect benefic asupra sistemului vascular, reduce sarcina asupra inimii, ajută la combaterea insuficienței cardiace și a durerii în cazul anginei pectorale.",
+
+"lt68": "<span class=\"red-bck\">XX </span>NORMALIZEAZĂ PRESIUNEA DVS., AVÂND O ACȚIUNE ÎN COMPLEX ASUPRA ORGANISMULUI ",
+
+"lt69": "ACȚIUNEA DE BAZĂ:",
+
+"lt70": " <b>Previne </b>atacurile de cord și accidentele vascular-cerebrale",
+
+"lt71": " <b>Normalizează</b> tensiunea arterială ",
+
+"lt72": " <b>Elimină </b>spasmul vaselor",
+
+"lt73": " <b>Întărește </b>pereții vaselor",
+
+"lt74": " <b>Îmbunătățește </b>indicatorii colesterolului",
+
+"lt75": "ACȚIUNI SUPLIMENTARE:",
+
+"lt76": " <b>Stabilizează </b>nivelul zahărului în sânge",
+
+"lt77": " <b>Întărește </b>imunitatea",
+
+"lt78": " <b>Elimină </b>anxietatea",
+
+"lt79": " <b>Normalizează </b>metabolismul",
+
+"lt80": " <b>Restabilește </b>funcția erectilă",
+
+"lt81": "Comandă",
+
+"lt82": "Institutul de Boli Cardiovasculare în 2019 a testat <span class=\"red-bck\">XX</span><br> pe două grupuri de voluntari de vârste diferite care sufereau de hipertensiune arterială.<br> Primul grup de referință a primit placebo, iar al doilea — <span class=\"red-bck\">XX</span> .",
+
+"lt83": "Rezultatele cercetării",
+
+"lt84": "PLACEBO",
+
+"lt86": "Stabilizarea tensiunii arteriale în limite normei în câteva ore după prima administrare",
+
+"lt89": "Lipsa atacurilor de hipertensiune arterială timp de două săptămâni de la administrarea unui curs",
+
+"lt92": "Lipsa simptomelor de hipertensiune arterială timp de trei luni de la finisării cursului",
+
+"lt95": "PĂREREA MEDICULUI SPECIALIST",
+
+"lt96": "„Fiecare a cincea persoană suferă de hipertensiune arterială și fiecare a doua persoană după 50 de ani. Adesea, persoanele nu observă această stare și o lasă fără atenția corespunzătoare, în alte cazuri, pur și simplu, se tratatează incorect. De aici, statisticile terifiante ale mortalității cauzate de boli cardiovasculare la nivel mondial — 40% din toate decesele.",
+
+"lt97": "Problema cu majoritatea medicamentelor hipotensive existente este că acestea provoacă o scădere bruscă a presiunii. Acest lucru are un efect devastator asupra sănătății vaselor sangvine și a inimii. Mai mult, aceste medicamente trebuie să fie administrate în mod constant.",
+
+"lt98": " <span>XX</span> este un remediu unic care nu creează dependență și fără efecte secundare. Scade ușor tensiunea arterială și acționează asupra cauzelor hipertensiunii arteriale și nu doar „suprima” simptomele.",
+
+"lt99": "Desigur, recomand acest remediu tuturor pacienților mei cu această problemă. Pentru consolidarea pe termen lung a rezultatului, va fi necesară abandonarea obiceiurilor proaste și trecerea la un stil de viață sănătos. Făcând acest lucru, nu numai că veți deveni mai sănătos, dar veți uita și de problemele cu presiunea timp de mai mulți ani”.",
+
+"lt100": "Galina Belova, medic-cardiolog, experiență de muncă 21 de ani",
+
+"lt101": "RECENZIILE CLIENȚILOR",
+
+"lt102": "XX a soluționat toate problemele. Datorită stresului constant la locul de muncă, tensiunea arterială a crescut în mod regulat. Am încercat o mulțime de mijloace, dar nu au acționat la distanță. Cu XX însă am început în sfârșit să adorm și să mă trezesc în mod normal  nu mai sufer de anxietate. Dimineața nu-mi mai amintesc de slăbiciune și greață sau despre durerile teribile de cap. Presiunea acum este întotdeauna normală. Categoric, recomand tuturor! <div class=\"slide-signa\">Iulia, 41 ani</div> ",
+
+"lt103": "Cu hipertonia eram cunoscută de mai mulți ani. Chiar mă obișnuisem să conviețuiesc cu ea. Dar, după cum spun profesioniștii, nu trebuie de suportat nici într-un caz presiunea, așa că o tratam. Mai devreme sau mai târziu, la toate medicamentele se creează dependență, așa că eram nevoită permanent să schimb schema de tratament. Odată mi-a nimerit în mâini XX. Niciodată nu m-am simțit așa de ușor! Cel mai plăcut este că are un efect de lungă durată! <div class=\"slide-signa\">Margareta, 53 ani</div> ",
+
+"lt104": "Un remediu excelent! 4 ani nu am putut face nimic cu presiunea, care se ridica brusc.  Evident că m-am adresat medicilor, administram pastile. Imediat ce încetam să le iau, presiunea revenea. Eu însă nu puteam să traiesc permanent cu pastile, am ficatul slab. Un medic mi-a recomandat XX, deoarece are o compoziție naturală, și nu influențează negativ asupra organelor interne. Cursul l-am finisat deja de cinci luni, presiunea nu s-a mai ridicat. <div class=\"slide-signa\">Nicolae, 58 ani</div> ",
+
+"lt109": "pentru a învinge rapid hipertonia și simptomele ei",
+
+"lt110": " <span>Normalizează </span>tensiunea în mod natural",
+
+"lt111": " <span>Restabilește</span> tonusul și elasticitatea vasculară",
+
+"lt112": " <span>Previne </span>atacurile de cord și accidentele vascular-cerebrale",
+
+"lt113": " <span>Este eficient</span> la orice etapă a hipertoniei",
+
+"lt114": "OFERTĂ SPECIALĂ",
+
+"lt115": "până la sfârșitul promoției a mai rămas",
+
+"lt116": "COMANDĂ",
+
+"lt117": "CUM LUCRĂM",
+
+"lt118": "I PAS",
+
+"lt119": "Lăsați comanda <br>pe site-ul nostru",
+
+"lt120": "AL II-LEA PAS",
+
+"lt121": "Operatorul vă va contacta <br>pentru a confirma comanda",
+
+"lt122": "AL III-LEA PAS",
+
+"lt123": "Achitarea va avea loc la recepționarea <br>comenzii ",
+
+
+
+
+ 
+
+
+ 
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
