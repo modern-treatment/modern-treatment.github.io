bff7b0c26b647359e5962b59df87814d3d27747b
vlad <vlad.bras02@gmail.com>
2023-02-28 20:48:23 +0100

    Cardione CZ added

---
+++ 2e548588/js/translater.js
@@ -0,0 +1,241 @@
+var new_lang = {
+    
+"lt0": "<img alt=\"icon\" src=\"img/icon1.png\">Schválen špičkovými<br>kardiology",
+
+"lt1": "<img alt=\"icon\" src=\"img/icon2.png\">100% přírodní<br>složení",
+
+"lt2": "<img alt=\"icon\" src=\"img/icon3.png\">Klinicky<br>dokázaný účinek",
+
+"lt3": "pro rychlé vítězství nad hypertenzí a jejími příznaky",
+
+"lt4": " <span>Reguluje</span> tlak přirozeným způsobem",
+
+"lt5": " <span>Navrací</span> cévám tonus a elasticitu",
+
+"lt6": " <span>Předchází</span> infarktům a mrtvicím",
+
+"lt7": " <span>Je účinný</span> v jakémkoli stádiu hypertenze",
+
+"lt8": "Objednat",
+
+"lt9": "SPECIÁLNÍ NABÍDKA",
+
+"lt10": "do konce akce zbývá",
+
+"lt11": "OBJEDNAT",
+
+"lt12": "PŘÍZNAKY ZAČÍNAJÍCÍ HYPERTENZE",
+
+"lt13": "problémy se<br> spánkem",
+
+"lt14": "poruchy<br> paměti",
+
+"lt15": "ranní<br> otoky",
+
+"lt16": "bolesti hlavy a<br> závratě",
+
+"lt17": "chronická<br> únava",
+
+"lt18": "dušnost, <br>ostrá bolest v hrudi",
+
+"lt19": "zvonění v uších<br><br>",
+
+"lt20": "úzkost,<br> podrážděnost",
+
+"lt21": "zvýšené<br> pocení",
+
+"lt22": "VYSOKÝ TLAK — HROZBA<br> PRO CELÝ ORGANIZMUS",
+
+"lt23": "SRDCE",
+
+"lt24": "kardiomegalie<i></i>",
+
+"lt25": "kardiomyopatie<i></i>",
+
+"lt26": "infarkt myokardu<i></i>",
+
+"lt27": "JÁTRA",
+
+"lt28": "cirhóza<i></i>",
+
+"lt29": "narušení metabolizmu<i></i>",
+
+"lt30": "zhoršení funkcionálního stavu<i></i>",
+
+"lt31": "SYSTÉM<br> KREVNÍHO OBĚHU",
+
+"lt32": "ztenčení stěn<br> cév a arterií<i></i>",
+
+"lt33": "diabetes<i></i>",
+
+"lt34": "MOZEK",
+
+"lt35": "<i></i>cévní mozková příhoda",
+
+"lt36": "<i></i>zhoršení paměti a intelektu",
+
+"lt37": "<i></i>mrtvice",
+
+"lt38": "ZRAK",
+
+"lt39": "<i></i>oddělení sítnice",
+
+"lt40": "<i></i>krevní výron",
+
+"lt41": "<i></i>slepota",
+
+"lt42": "LEDVINY",
+
+"lt43": "<i></i>nefroskleróza",
+
+"lt44": "<i></i>selhání ledvin",
+
+"lt45": "<span class=\"red-bck\">XX</span> ODSTRAŇUJE<span class=\"color-red\">4 ZÁKLADNÍ</span>PŘÍČINY<br>HYPERTENZE",
+
+"lt46": "NERVOVÉ NAPĚTÍ",
+
+"lt47": "Bojuje s úzkostí a reguluje režim spánku a bdělého stavu",
+
+"lt48": "ZNEČIŠTĚNÍ CÉV",
+
+"lt49": "Čistí cévní stěny od cholesterových usazenin, předchází ateroskleróze",
+
+"lt50": "Zvýšená hladina cukru<br> v krvi",
+
+"lt51": "Snižuje množství cukru v krvi — předchází diabetu",
+
+"lt52": "NADVÁHA",
+
+"lt53": "Napomáhá normalizaci hmotnosti díky zlepšení látkové výměny",
+
+"lt54": "Objednat",
+
+"lt55": "ROSTLINNÉ SLOŽENÍ<span class=\"red-bck\">XX</span>OBSAHUJE<br> SLOŽKY S VYSOKOU BIOLOGICKOU DOSTUPNOSTÍ:",
+
+"lt56": "THYAMIN",
+
+"lt57": "zlepšuje srdeční frekvenci a zajišťuje zdraví myokardu. Předchází anémii, stabilizuje výměnu tuků a zajišťuje normální fungování mozkových cév a očí.",
+
+"lt58": "SELEN",
+
+"lt59": "nejen, že chrání cévy před vznikem aterosklerózy, ale bezprostředně samotný srdeční sval před škodlivým vlivem volných radikálů.",
+
+"lt60": "VITAMÍN C",
+
+"lt61": "záruka elasticity cévních stěn. Díky jeho vlivu nedochází k trhlinám a křehkosti. Také zlepšuje srážlivost krve, urychluje regeneraci tkání.",
+
+"lt62": "VRCHOLÁK",
+
+"lt63": "silné tonikum pro srdce, tonizuje srdce a plíce, stimuluje koronární krevní oběh, posiluje srdeční sval.",
+
+"lt64": "VÝTAŽEK ZE SEMEN HROZNA",
+
+"lt65": "stimuluje fungování srdce a krevního systému, snižuje riziko mrtvice. Zpevňuje cévní stěny a zvyšuje jejich elasticitu.",
+
+"lt66": "FYTOVÝTAŽKY",
+
+"lt67": "blahodárně účinkují na cévní systém, snižují zatížení srdce, pomáhají se vyrovnat se srdeční slabostí a bolestí při angíně pectoris.",
+
+"lt68": "<span class=\"red-bck\">XX</span> NORMALIZUJE VÁŠ TLAK, KDY KOMPLEXNĚ PŮSOBÍ NA ORGANIZMUS",
+
+"lt69": "ZÁKLADNÍ ÚČINEK:",
+
+"lt70": " <b>Předchází</b> infarktu a mrtvici",
+
+"lt71": " <b>Normalizuje</b> arteriální tlak",
+
+"lt72": " <b>Odstraňuje cévní</b> křeč",
+
+"lt73": " <b>Zpevňuje</b> cévní stěny",
+
+"lt74": " <b>Zlepšuje</b> ukazatele krevního cholesterolu",
+
+"lt75": "DALŠÍ ÚČINKY:",
+
+"lt76": " <b>Stabilizuje</b> úroveň cukru v krvi",
+
+"lt77": " <b>Posiluje</b> imunitu",
+
+"lt78": " <b>Odstraňuje</b> podrážděnost",
+
+"lt79": " <b>Normalizuje</b> látkovou výměnu",
+
+"lt80": " <b>Obnovuje</b> erektivní funkci",
+
+"lt81": "Objednat",
+
+"lt82": "Institut kardiovaskulárních onemocnění provedl v roce 2019 testování<span class=\"red-bck\">XX</span><br> na dvou skupinách dobrovolníků různého věku, trpících hypertenzí. <br>První testovaná skupina dostávala placebo, druhá — <span class=\"red-bck\">XX</span> .",
+
+"lt83": "Výsledky testování",
+
+"lt84": "PLACEBO",
+
+"lt86": "Stabilizace arteriálního tlaku v mezích normy během několika hodin po prvním užití",
+
+"lt89": "Absence záchvatů hypertenze během dvou týdnů užívání ve formě kúry",
+
+"lt92": "Absence příznaků hypertenze během tří měsíců po ukončení užívání ve formě kúry",
+
+"lt95": "MÍNĚNÍ LÉKAŘE SPECIALISTY",
+
+"lt96": "„Arteriální hypertenzí trpí každý pátý, po 50 roku — každý druhý člověk. Často si lidé tohoto stavu nevšimnou a nechávají ho bez pozornosti, v jiných případech se prostě léčí nesprávně. Odtud pochází hrozná statistika úmrtnosti na kardiovaskulární onemocnění na celém světě — 40% ze všech úmrtí.",
+
+"lt97": "Problém většiny existujících hypotenzívních preparátů spočívá v tom, že způsobují výrazné snížení tlaku. To se devastujícím způsobem odráží na zdraví cév a srdce. Navíc, užívat takové preparáty je třeba neustále.",
+
+"lt98": " <span>XX</span> — unikátní prostředek,který nezpůsobuje závislost a nemá vedlejší účinky. Postupně snižuje tlak a směřuje na příčiny hypertenze, nejen pouze „potlačuje“ příznaky.",
+
+"lt99": "Bezvýhradně tento produkt doporučuji všem svým pacientům s tímto problémem. Pro dlouhodobé uchování výsledku bude třeba se zbavit zlozvyků a vést zdravý způsob života. Pokud to uděláte, nejen že budete zdravější, ale i zapomenete na problémy se zdravím na dlouhé roky“.",
+
+"lt100": "Helena Bílá, lékař kardiolog, pracovní stáž 21 let",
+
+"lt101": "OHLASY KLIENTŮ",
+
+"lt102": "XX vyřešil všechny mé problémy. Díky neustálým pracovním stresům se mi pravidelně zvyšoval tlak. Zkusila jsem hromadu prostředků, ale všechny působily jen vzdáleně. A s XX jsem nakonec začala normálně usínat a probouzet se, vyrovnala jsem se s úzkostí. Na slabost a ranní nevolnost si ani nevzpomínám, stejně jako na hrozné bolesti hlavy. Tlak je nyní stále v normě. Jednoznačně všem doporučuji! <div class=\"slide-signa\">Julie, 41 let</div> ",
+
+"lt103": "S hypertenzí jsem se znala mnoho let. Jako bych se s ní dokonce naučila žít. Ale, jak říkají profesionálové, snášet tlak v žádném případě nelze, bylo třeba se léčit. Na všechny preparáty jsem si dříve nebo později zvykla, takže jsem byla nucena neustále měnit schéma léčby. Jednou se mi do rukou dostal XX. Tak lehce jsem se nikdy necítila! Nejlepší je, že efekt je dlouhodobý! <div class=\"slide-signa\">Markéta, 53 let</div> ",
+
+"lt104": "Výborný prostředek! 4 roky jsem nemohl s tlakem nic udělat, pořád se zvyšoval. Samosebou jsem chodil k lékařům, užíval tablety. Ale jak jsem přestal, tlak se vracel. A já nemohu neustále žít na lécích — mám slabá játra. Lékař mi poradil XX, jelikož má přírodní složení a neškodí vnitřním orgánům. Kúru jsem skončil již před pěti měsíci, tlak se více nevrátil. <div class=\"slide-signa\">Pavel, 58 let</div> ",
+
+"lt109": "pro rychlé vítězství nad hypertenzí a jejími příznaky",
+
+"lt110": " <span>Reguluje</span> tlak přirozeným způsobem",
+
+"lt111": " <span>Navrací</span> cévám tonus a elasticitu",
+
+"lt112": " <span>Předchází</span> infarktům a mrtvicím",
+
+"lt113": " <span>Je účinný</span> v jakémkoli stádiu hypertenze",
+
+"lt114": "SPECIÁLNÍ NABÍDKA",
+
+"lt115": "do konce akce zbývá",
+
+"lt116": "OBJEDNAT",
+
+"lt117": "JAK PRACUJEME",
+
+"lt118": "1. KROK",
+
+"lt119": "Zanechte požadavek na<br>našich stránkách",
+
+"lt120": "2. KROK",
+
+"lt121": "Operátor se s Vámi spojí<br>pro potvrzení objednávky",
+
+"lt122": "3. KROK",
+
+"lt123": "Platba při doručení<br> objednávky",
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
