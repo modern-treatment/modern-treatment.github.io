529eb2bd85276a9b2403caef5159bef18f99cb1e
vlad <vlad.bras02@gmail.com>
2023-03-02 13:09:21 +0100

    Cardione SK 2

---
+++ 41a23ecb/js/translater.js
@@ -0,0 +1,247 @@
+var new_lang = {
+    
+"lt0": "<img alt=\"icon\" src=\"img/icon1.png\">Schválené poprednými<br> kardiológmi",
+
+"lt1": "<img alt=\"icon\" src=\"img/icon2.png\">100% prírodné <br>zloženie",
+
+"lt2": "<img alt=\"icon\" src=\"img/icon3.png\">Klinicky<br> preukázaný účinok",
+
+"lt3": "pre rýchle víťazstvo nad hypertenziou a jej príznakmi",
+
+"lt4": " <span>Prirodzené</span> normalizuje tlak",
+
+"lt5": " <span>Obnovuje</span> vaskulárny tonus a elasticitu",
+
+"lt6": " <span>Zabraňuje</span> infarktom a mozgovým mŕtviciam",
+
+"lt7": " <span>Účinné</span> v akomkoľvek štádiu hypertenzie",
+
+"lt8": "Objednať",
+
+"lt9": "ŠPECIÁLNA PONUKA",
+
+"lt10": "do konca promočnej akcie",
+
+"lt11": "OBJEDNAŤ",
+
+"lt12": "PRÍZNAKY DOPOSIAĽ NEODHALENEJ HYPERTENZIE",
+
+"lt13": "problémy so<br> spánkom",
+
+"lt14": "poškodenie<br> pamäte",
+
+"lt15": "opuch v<br> dopoludňajších hodinách",
+
+"lt16": "bolesti hlavy a <br>závraty",
+
+"lt17": "chronická <br>únava",
+
+"lt18": "dýchavičnosť,<br> ostrá bolesť na hrudníku",
+
+"lt19": "tinnitus<br><br>",
+
+"lt20": "úzkosť, <br>podráždenosť",
+
+"lt21": "zvýšené <br>potenie",
+
+"lt22": "ZVÝŠENÝ TLAK HROZÍ <br>CELÉMU ORGANIZMU!",
+
+"lt23": "SRDCE",
+
+"lt24": "kardiomegaliya<i></i>",
+
+"lt25": "kardiomyopatia<i></i>",
+
+"lt26": "infarkt myokardu<i></i>",
+
+"lt27": "PEČEŇ",
+
+"lt28": "cirhóza<i></i>",
+
+"lt29": "metabolická porucha<i></i>",
+
+"lt30": "poškodenie funkci organizmu<i></i>",
+
+"lt31": "KRVNÝ<br> SYSTÉM",
+
+"lt32": "riedenie stien krvných <br>ciev a tepien<i></i>",
+
+"lt33": "cukrovka<i></i>",
+
+"lt34": "MOZOG",
+
+"lt35": "<i></i>cerebrovaskulárna príhoda",
+
+"lt36": "<i></i>strata pamäti a inteligencie",
+
+"lt37": "<i></i>mozgová príhoda",
+
+"lt38": "ZRAK",
+
+"lt39": "<i></i>oddelenie sietnice",
+
+"lt40": "<i></i>krvácanie",
+
+"lt41": "<i></i>slepota",
+
+"lt42": "OBLIČKY",
+
+"lt43": "<i></i>nefroskleróza",
+
+"lt44": "<i></i>zlyhanie obličiek",
+
+"lt45": "<span class=\"red-bck\">XX</span> VYROVNÁVA<span class=\"color-red\"> 4 ZÁKLADNÉ</span> PRÍČINY <br>HYPERTENZIE",
+
+"lt46": "NÉRVOVÉ NAPÄTIE",
+
+"lt47": "Bojuje proti úzkosti a reguluje spánok a bdelosť",
+
+"lt48": "VASKULÁRNA TROSKA",
+
+"lt49": "Čistí steny krvných ciev od cholesterolových plakov, zabraňuje ateroskleróze",
+
+"lt50": "Vysoká hladina cukru<br> v krvi",
+
+"lt51": "Znižuje hladinu cukru v krvi, prevencia cukrovky",
+
+"lt52": "NADVÁHA",
+
+"lt53": "Pomáha normalizovať hmotnosť zlepšením metabolizmu",
+
+"lt54": "Objednať",
+
+"lt55": "RASTLINNÁ KOMPOZÍCIA <span class=\"red-bck\">XX</span> JE ZLOŽENÁ<br> Z KOMPONENTOV S VYSOKOU BIOLOGICKOU DOSTUPNOSŤOU:",
+
+"lt56": "TIAMÍN",
+
+"lt57": "zlepšuje kontrakcie srdca a zaisťuje zdravie myokardu. Zabraňuje anémii, stabilizuje metabolizmus tukov a zaisťuje normálne fungovanie ciev mozgu a očí.",
+
+"lt58": "SELÉN",
+
+"lt59": "chráni nielen cievy pred výskytom aterosklerózy, ale priamo samotný srdcový sval pred škodlivými účinkami voľných radikálov.",
+
+"lt60": "VITAMÍN C",
+
+"lt61": "záruka pružnosti stien krvných ciev. Vďaka tejto činnosti nedochadza k trhlinam v cievach a ich krehkosi. Zlepšuje tiež zrážanie krvi, urýchľuje regeneraciu tkanív.",
+
+"lt62": "ARJUN",
+
+"lt63": "silné tonikum pre srdce, tónuje srdce a pľúca, stimuluje koronárny obeh, posilňuje srdcový sval.",
+
+"lt64": "VÝŤAHA Z HROZNOVÝCH SEMIEN",
+
+"lt65": "Stimuluje srdce a obehový systém, znižuje riziko mozgovej príhody. Spevňuje steny krvných ciev a zvyšuje ich elasticitu.",
+
+"lt66": "BYLINNÉ EXTRAKTY",
+
+"lt67": "majú priaznivý účinok na cievny systém, znižujú záťaž srdca, pomáha vyrovnať sa so srdcovým zlyhaním a bolestiam pri angíne pectoris.",
+
+"lt68": "<span class=\"red-bck\">XX</span> NORMALIZUJE TLAK, MÁ SKUTOČNÝ VPLYV NA CELÝ ORGANIZMUS",
+
+"lt69": "HLAVNÉ PÔSOBENIE:",
+
+"lt70": " <b>Zabraňuje</b> infarktom a mozgovým mŕtviciam",
+
+"lt71": " <b>Normalizuje</b> krvný tlak",
+
+"lt72": " <b>Zmierňuje kŕče krvných</b> ciev",
+
+"lt73": " <b>Spevňuje </b>steny krvných ciev",
+
+"lt74": " <b>Zlepšuje </b>hladinu cholesterolu v krvi",
+
+"lt75": "ĎALŠIE ÚČINKY:",
+
+"lt76": " <b>Stabilizuje</b> hladinu cukru v krvi",
+
+"lt77": " <b>Posilňuje</b> imunitný systém",
+
+"lt78": " <b>Zmierňuje</b> podráždenosť",
+
+"lt79": " <b>Normalizuje</b> metabolizmus",
+
+"lt80": " <b>Obnovuje</b> funkciu erekcie",
+
+"lt81": "Objednať",
+
+"lt82": "V roku 2019 Inštitút kardiovaskulárnych chorôb testoval <span class=\"red-bck\">XX</span><br> na dvoch skupinách dobrovoľníkov rôzneho veku trpiacich hypertenziou. <br>Prvá testová skupina dostala placebo a druhá — <span class=\"red-bck\">XX</span> .",
+
+"lt83": "Výsledky výskumu",
+
+"lt84": "PLACEBO",
+
+"lt86": "Stabilizácia krvného tlaku v rámci normálnych limitov niekoľko hodín po prvej aplikácii",
+
+"lt89": "Absencia záchvatov hypertenzie do dvoch týždňov od príjmu v priebehu kurzu",
+
+"lt92": "Absencia príznakov hypertenzie do troch mesiacov po ukončení kurzu",
+
+"lt95": "NÁZOR ŠPECIALIZAČNÉHO LEKÁRA",
+
+"lt96": "„Každá piata osoba trpí arteriálnou hypertenziou a každá druhá osoba po 50 rokoch. Ľudia si často tento stav nevšimnú a ponechajú ho bez dozoru, v iných prípadoch sa s ním jednoducho zaobchádzajú nesprávne. Preto sú desivé štatistiky úmrtnosti na kardiovaskulárne choroby na celom svete — 40% všetkých úmrtí.",
+
+"lt97": "Problém s väčšinou existujúcich hypotenzívnych liekov je v tom, že spôsobujú prudký pokles tlaku. To má ničivý vplyv na zdravie krvných ciev a srdca. Navyše musíte brať také lieky neustále.",
+
+"lt98": " <span>XX</span> je jedinečný produkt, ktorý nie je návykový a nemá vedľajšie účinky. Jemne znižuje krvný tlak a pôsobí na príčiny hypertenzie a nielen „potláča“ príznaky.",
+
+"lt99": "Tento nástroj, samozrejme, odporúčam všetkým svojim pacientom s týmto problémom. Pre dlhodobú konsolidáciu výsledku bude potrebné zanechať zlé návyky a viesť zdravý životný štýl. Týmto sa nielenže stanete zdravším, ale aj zabudnete na problémy s tlakom na mnoho rokov“.",
+
+"lt100": "Karolína Gálová, kardiológ, pracovná prax 21 rokov",
+
+"lt101": "NÁZORY ZÁKAZNÍKOV",
+
+"lt102": "XX vyriešil všetky moje problémy. Kvôli neustálemu stresu pri práci sa krvný tlak pravidelne zvyšoval. Skúsila som veľa liekov, ale nepomáhali. A s XX som konečne normálne zaspala a zobudila sa, zvládla som úzkosť. Ráno si už nepamätám na slabosť a nevoľnosť a tiež strašné bolesti hlavy. Tlak je teraz vždy normálny. Rozhodne to odporúčam všetkým! <div class=\"slide-signa\">Helena, 41 rok</div> ",
+
+"lt103": "Trpela som hypertenziou už mnoho rokov. Vyzerá to, že som sa už naučila žiť s ňou. Ale ako hovoria odborníci, v každom prípade nie je možné vydržovať vysoký tlak, a musela som ho začať liečiť. Všetky lieky sa skôr stali návykovými, takže som bola neustále nútená meniť liečebný režim. Akonáhle dostala som do rúk XX. Tak ľahké som sa nikdy necítila! Najlepšie na tom je dlhodobý efekt! <div class=\"slide-signa\">Miroslava, 53 rokov</div> ",
+
+"lt104": "Skvelý liek! 4 roky nemohol som urobiť nič s tlakom, ktorý stále zvyšoval. Samozrejme chodil som k lekárom a pil som pilulky. Akonáhle sa však prestaval som chodiť k lekárom a piť pilulky — tlak sa zvyšoval. Ale nemôžem neustále žiť s liekmi — mám slabú pečeň. Lekár odporučil XX, pretože má prírodné zloženie a nepoškodzuje vnútorné orgány. Zakončil som kurz päť mesiacov dozadu, tlak sa nezvyšuje. <div class=\"slide-signa\">Roman, 58 rokov</div> ",
+
+"lt109": "pre rýchle víťazstvo nad hypertenziou a jej príznakmi",
+
+"lt110": " <span>Prirodzené</span> normalizuje tlak",
+
+"lt111": " <span>Obnovuje</span> vaskulárny tonus a elasticitu",
+
+"lt112": " <span>Zabraňuje</span> infarktom a mozgovým mŕtviciam",
+
+"lt113": " <span>Účinné</span> v akomkoľvek štádiu hypertenzie",
+
+"lt114": "ŠPECIÁLNA PONUKA",
+
+"lt115": "do konca promočnej akcie",
+
+"lt116": "OBJEDNAŤ",
+
+"lt117": "AKO PRACUJEME",
+
+"lt118": "1 KROK",
+
+"lt119": "Zanechajte žiadosť na <br>našom webe",
+
+"lt120": "2 KROK",
+
+"lt121": "Operátor vás bude kontaktovať, <br>aby potvrdiť objednávku",
+
+"lt122": "3 KROK",
+
+"lt123": "Platba pri prijatí <br>objednávky",
+
+
+
+
+
+
+
+ 
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
